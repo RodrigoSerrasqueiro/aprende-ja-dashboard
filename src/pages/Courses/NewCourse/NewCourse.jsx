c7b2ae4a3c85559e428c80b03f71840cd34de7d6
Rodrigo Serrasqueiro <rodrigoserrasqueiro@gmail.com>
2023-07-17 14:42:55 -0300

    feat(integration api)

--- src/pages/Courses/NewCourse/NewCourse.jsx
+++ src/pages/Courses/NewCourse/NewCourse.jsx
@@ -1,14 +1,37 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { GlobalContext } from "../../../contexts/GlobalContext"
+import { ApiContext } from "../../../contexts/ApiContext"
 import { NewCourseContainer } from "./NewCourse.style"
 
 function NewCourse() {
 
   const { openMenu } = useContext(GlobalContext)
+  const { createCourse } = useContext(ApiContext)
+
+  const [courseType, setCourseType] = useState("");
+  const [courseSubType, setCourseSubType] = useState("");
+  const [courseName, setCourseName] = useState("");
+  const [courseDescription, setCourseDescription] = useState("");
+
+  const handleCourse = () => {
+    createCourse(courseType, courseSubType, courseName, courseDescription)
+  }
 
   return (
     <NewCourseContainer openmenu={openMenu ? "true" : "false"}>
-      Novo curso
+      <span>TIPO DE CURSO</span>
+      <input type="text" onChange={(e) => setCourseType(e.target.value)} />
+
+      <span>SUBTIPO DE CURSO</span>
+      <input type="text" onChange={(e) => setCourseSubType(e.target.value)} />
+
+      <span>NOME DO CURSO</span>
+      <input type="text" onChange={(e) => setCourseName(e.target.value)} />
+
+      <span>DESCRIÇÃO DO CURSO</span>
+      <input type="text" onChange={(e) => setCourseDescription(e.target.value)} />
+
+      <button onClick={handleCourse}>CADASTRAR CURSO</button>
     </NewCourseContainer>
   )
 }
--- src/pages/Courses/NewCourse/NewCourse.style.js
+++ src/pages/Courses/NewCourse/NewCourse.style.js
@@ -4,8 +4,27 @@ export const NewCourseContainer = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   background-color: ${({theme}) => theme.primaryColor};
   color: ${({theme}) => theme.primaryTextColor};
   padding: ${({openmenu}) => (openmenu === "true" ? "75px 0 0 310px" : "75px 0 0 10px" )};
   transition: .8s;
+
+  > input {
+    width: 300px;
+    height: 35px;
+    font-size: 18px;
+  }
+  > button {
+    width: 100px;
+    height: 35px;
+    margin-top: 20px;
+  }
+
+  &:hover {
+    > button {
+      cursor: pointer;
+    }
+  }
 `;
+
